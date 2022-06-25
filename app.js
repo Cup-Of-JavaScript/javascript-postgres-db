9cd75907cf8a8a5bc8ec15dada744748ae8e4218
Martin Burolla <mburolla@xpanxion.com>
2022-06-25 15:12:07 -0400

    update

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ import postgres from 'postgres'
 
 const sql = postgres('postgres://postgres:Ihgdp51505150!@localhost:5432/express')
 
-const main = async () => {
+const select = async () => {
     const result = await sql`select * from customer where customer_id = 1`
     console.log(result[0])
+}
+
+const insert = async () => {
+    const result = await sql`select * from customer where customer_id = 1`
+    console.log(result[0])
+}
+
+const main = async () => {
+    await select()
     process.exit();
 }
 
