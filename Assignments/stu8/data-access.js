a60e71a5cec617bcd4e7c17e86080c6c42b7a250
Martin Burolla <mburolla@xpanxion.com>
2022-06-26 08:15:03 -0400

    update

---
+++ Assignments/stu8/data-access.js
@@ -0,0 +1,30 @@
+//
+// File: data-access.js
+// Date: 6/25/2022
+//
+
+const ex13 = () => {
+    console.log('TODO...')
+}
+
+const ex14 = () => {
+    console.log('TODO...')
+}
+
+const ex15 = () => {
+    console.log('TODO...')
+}
+
+const ex16 = () => {
+    console.log('TODO...')
+}
+
+const ex17 = () => {
+    console.log('TODO...')
+}
+
+const main = () => {
+    ex10
+}
+
+main()
