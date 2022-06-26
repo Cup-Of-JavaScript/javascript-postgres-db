2fd307d4cdf05fdcceedf6b67cabb43c4aca18c1
Martin Burolla <mburolla@xpanxion.com>
2022-06-26 11:05:10 -0400

    update

--- Assignments/stu3/data-access.js
+++ Assignments/stu3/data-access.js
@@ -3,28 +3,81 @@
 // Date: 6/25/2022
 //
 
-const ex13 = () => {
-    console.log('TODO...')
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  password: "Ihgdp51505150!",
+  database: "express",
+  host: "localhost",
+  port: 5432,
+});
+
+const ex13 = async () => {
+    let personId = 1
+    console.log(await getPerson(personId))
 }
 
-const ex14 = () => {
-    console.log('TODO...')
+const ex14 = async () => {
+    let bookstoreId = 1
+    console.log(await getBooks(bookstoreId))
 }
 
-const ex15 = () => {
-    console.log('TODO...')
+const ex15 = async () => {
+    let personId = 1
+    let newName = "Johnny"
+    console.log(await updatePerson(personId, newName))
 }
 
-const ex16 = () => {
-    console.log('TODO...')
+const ex16 = async () => {
+    let bookstoreName = "Book World"
+    console.log(await addBookstore(bookstoreName))
 }
 
-const ex17 = () => {
-    console.log('TODO...')
+const ex17 = async () => {
+    let newBookTitle = "It Begins Here"
+    let newBookIsbn = "123-12-12344-22-111"
+    let bookStoreId = 1
+    console.log(await addBook(newBookTitle, newBookIsbn, bookStoreId))
 }
 
-const main = () => {
-    ex10
+const main = async () => {
+    await ex17()
+    process.exit()
+}
+
+//
+// Your functions here...
+//
+
+const getPerson = async (personId) => {
+    let retval = null;
+    // TODO...
+    return retval;
+}
+
+const getBooks = async (bookStoreId) => {
+    let retval = null;
+    // TODO...
+    return retval;
+}
+
+const updatePerson = async (personId, newName) => {
+    let retval = null;
+    // TODO...
+    return retval;
+}
+
+const addBookstore = async (bookstoreName) => {
+    let retval = null;
+    // TODO...
+    return retval;
+}
+
+const addBook = async (title, isbn, bookstoreId) => {
+    let retval = null;
+    // TODO...
+    return retval;
 }
 
 main()
