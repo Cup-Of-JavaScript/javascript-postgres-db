e6261722f6727c70c69fa4827285e9ad9fe2f6b8
Kelsey Perkins <kperkins@xpanxion.com>
2022-07-05 11:48:22 -0700

    ex 5 query

--- Assignments/stu7/queries.sql
+++ Assignments/stu7/queries.sql
@@ -20,9 +20,12 @@ SELECT first_name, last_name FROM person ORDER BY last_name DESC
  * Ex. 4
  */
 SELECT person_id, first_name, last_name, dob FROM person WHERE dob BETWEEN  '1970-08-29' AND '1979-07-10' ;
+
 /**
  * Ex. 5
  */
+
+UPDATE person SET first_name = 'John' WHERE person_id=9
 
 /**
  * Ex. 6
