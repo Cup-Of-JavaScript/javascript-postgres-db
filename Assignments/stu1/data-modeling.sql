1eb03e53cc0595c0c0d6e6684c920b0dc4a2a4e9
Adraina Cheeks <programmingwithacheeks@xpanxion.com>
2022-07-07 18:12:46 -0400

    updating table 6

--- Assignments/stu1/data-modeling.sql
+++ Assignments/stu1/data-modeling.sql
@@ -13,20 +13,26 @@ insert into size (lemonade_size, lemonade_price) values ('Large', '$3.00');
 insert into lemonade (lemonade_flavor) values ('Lemon');
 insert into lemonade (lemonade_flavor) values ('Strawberry');
 
-select (current_date, count(date)) from lemonade;
+select * from lemonade
+     where date > now() - interval '1 day';
+     
+     insert into lemonade(lemonade_size_id, lemonade_flavor, date, sales) 
+     values(6, 'Strawberry','2020-07-07',200)
 
 CREATE TABLE IF NOT EXISTS public.lemonade
 (
     lemonade_id serial NOT NULL,
     lemonade_size_id integer,
-    lemonade_flavors "char",
+    lemonade_flavors "char" DEFAULT 50,
+    lemonade_total integer,
+    date timestamp without time zone,
     PRIMARY KEY (lemonade_id)
 );
 
 CREATE TABLE IF NOT EXISTS public.size
 (
     size_id serial NOT NULL,
-    lemonade_size "char",
+    lemonade_size "char" DEFAULT 50,
     lemonade_price money,
     PRIMARY KEY (size_id)
 );
