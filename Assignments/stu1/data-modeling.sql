a074a584614f58a0a7bd638a56757ed211fc63a3
Adraina Cheeks <programmingwithacheeks@xpanxion.com>
2022-07-07 14:46:32 -0400

    updating table 2

--- Assignments/stu1/data-modeling.sql
+++ Assignments/stu1/data-modeling.sql
@@ -5,7 +5,6 @@ BEGIN;
 
 select * from lemonade;
 select * from size;
-select * from logging.t_history;
 
 insert into size (lemonade_size, lemonade_price) values ('Small', '$1.00');
 insert into size (lemonade_size, lemonade_price) values ('Medium', '$2.00');
@@ -13,6 +12,8 @@ insert into size (lemonade_size, lemonade_price) values ('Large', '$3.00');
 
 insert into lemonade (lemonade_flavor) values ('Lemon');
 insert into lemonade (lemonade_flavor) values ('Strawberry');
+
+select sum(lemonade_size_id) from lemonade as date;
 
 CREATE TABLE IF NOT EXISTS public.lemonade
 (
@@ -29,40 +30,6 @@ CREATE TABLE IF NOT EXISTS public.size
     lemonade_price money,
     PRIMARY KEY (size_id)
 );
-
-CREATE SCHEMA logging;
-CREATE TABLE logging.t_history (
-id   serial,
-tstamp   timestamp DEFAULT now(),
-schemaname   text,
-tabname   text,
-operation   text,
-who   text   DEFAULT current_user,
-new_val   json,
-old_val json
-);
-
-CREATE FUNCTION change_trigger() RETURNS trigger AS $$
-BEGIN
-IF TG_OP = 'INSERT'
-THEN
-INSERT INTO logging.t_history (tabname, schemaname, operation, new_val)
-VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP, row_to_json(NEW));
-RETURN NEW;
-ELSIF TG_OP = 'UPDATE'
-THEN
-INSERT INTO logging.t_history (tabname, schemaname, operation, new_val, old_val)
-VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP,
-row_to_json(NEW), row_to_json(OLD));
-RETURN NEW;
-ELSIF TG_OP = 'DELETE'
-THEN
-INSERT INTO logging.t_history (tabname, schemaname, operation, old_val)
-VALUES (TG_RELNAME, TG_TABLE_SCHEMA, TG_OP, row_to_json(OLD));
-RETURN OLD;
-END IF;
-END;
-$$ LANGUAGE 'plpgsql' SECURITY DEFINER;
 
 ALTER TABLE IF EXISTS public.lemonade
     ADD FOREIGN KEY (lemonade_size_id)
