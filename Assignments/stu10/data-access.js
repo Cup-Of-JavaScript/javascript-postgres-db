4278d97d1863c6c7bcc59c4ce7a06088cd1411e4
Sherika Fayson <sfayson@xpanxion.com>
2022-07-11 16:18:08 -0400

    Ex15 Update Person

--- Assignments/stu10/data-access.js
+++ Assignments/stu10/data-access.js
@@ -9,6 +9,7 @@ const GET_BOOKS= `select b.title
 from book b
 join book_store_book bsb on b.book_id=bsb.book_id 
 join book_store bs on bsb.book_store_id=bs.book_store_id where bs.book_Store_id = ($1);`
+const UPDATE_PERSON = "update person set first_name = $2 where person_id = $1 returning person_id, first_name"
 
 const pool = new Pool({
   user: "postgres",
@@ -28,26 +29,26 @@ exports.ex14 = async () => {
     return (await this.getBooks(bookstoreId))
 }
 
-const ex15 = async () => {
+exports.ex15 = async () => {
     let personId = 1
     let newName = "Johnny"
-    console.log(await updatePerson(personId, newName))
+    return (await this.updatePerson(personId, newName))
 }
 
-const ex16 = async () => {
+exports.ex16 = async () => {
     let bookstoreName = "Book World"
-    console.log(await addBookstore(bookstoreName))
+    return(await this.addBookstore(bookstoreName))
 }
 
-const ex17 = async () => {
+exports.ex17 = async () => {
     let newBookTitle = "It Begins Here"
     let newBookIsbn = "123-12-12344-22-111"
     let bookStoreId = 1
-    console.log(await addBook(newBookTitle, newBookIsbn, bookStoreId))
+    return(await this.addBook(newBookTitle, newBookIsbn, bookStoreId))
 }
 
 const main = async () => {
-    await ex14()
+    await ex15()
     process.exit()
 }
 
@@ -77,9 +78,14 @@ exports.getBooks = async (bookStoreId) => {
     return retval;
 }
 
-const updatePerson = async (personId, newName) => {
+exports.updatePerson = async (personId, newName) => {
     let retval = null;
-    // TODO...
+    try {
+        let r = await pool.query(UPDATE_PERSON, [personId, newName]);
+        retval = r.rows;
+      } catch (err) {
+        console.error(err);
+      }
     return retval;
 }
 
