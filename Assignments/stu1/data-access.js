acee7151e38293ec50d97387b9d89950bdaf3e0f
Adraina Cheeks <programmingwithacheeks@xpanxion.com>
2022-07-11 15:17:24 -0400

    adding new bookstore

--- Assignments/stu1/data-access.js
+++ Assignments/stu1/data-access.js
@@ -7,7 +7,7 @@ const { Pool } = require("pg");
 const GET_PERSON = "select * from person where person_id = $1"
 const GET_BOOK = "select bs.book_store_id, bs.book_store_name, b.title from book_store bs join book_store_book bb on bb.book_store_id = bs.book_store_id join book b on  b.book_id = bb.book_id where bs.book_store_id = $1"
 const UPDATE_PERSON = "update person set first_name = $2 where person_id = $1 returning person_id;"
-const ADD_BOOK = "insert into book_store (book_store_name) values ($1)returning book_store;"
+const ADD_BOOK = "insert into book_store (book_store_name) values ($1)returning book_store_id;"
 
 const pool = new Pool({
   user: "postgres",
