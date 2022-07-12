ce6d652011fbd319758ea9e5509071b92b835da6
steven davis <sdavis@xpanxion.com>
2022-07-12 13:42:25 -0700

    update

--- Assignments/stu9/data-access.js
+++ Assignments/stu9/data-access.js
@@ -4,41 +4,53 @@
 //
 
 const { Pool } = require("pg");
+//ex13
+const GET_PERSON = "select * from person where person_id =($1);"
+//ex14
+const GET_BOOKS= "select b.titlefrom book_store bsjoin book_store_book bsb on bsb.book_store_id = bs.book_store_idjoin book b on b.book_id=bsb.book_idwhere bs. book_store_id = ($1);"
+//ex15
+const updatePerson= "update person set first_name = $2 where person_id = $1 returning person_id;"
+//ex16
+const addBookstore = "insert into book_store (book_store_name) values ($1) returning book_store_name;"
+//ex17
+const addBook = "insert into book (title, isbn) values ($1, $2) returning book_id ;"
+const add_Book_Store_Book ="insert into book_store_book (book_id, book_store_id) values ($1, $2) returning book_store_book_id;"
+
 
 const pool = new Pool({
   user: "postgres",
-  password: "Ihgdp51505150!",
-  database: "express",
+  password: "Lamarjackson8",
+  database: "postgres",
   host: "localhost",
   port: 5432,
 });
 
-const ex13 = async () => {
+exports.ex13 = async () => {
     let personId = 1
-    console.log(await getPerson(personId))
+    console.log(await this.getPerson(personId))
 }
 
-const ex14 = async () => {
+exports.ex14 = async () => {
     let bookstoreId = 1
-    console.log(await getBooks(bookstoreId))
+    console.log(await this.getBooks(bookstoreId))
 }
 
-const ex15 = async () => {
+exports.ex15 = async () => {
     let personId = 1
     let newName = "Johnny"
-    console.log(await updatePerson(personId, newName))
+    console.log(await this.updatePerson(personId, newName))
 }
 
-const ex16 = async () => {
+exports.ex16= async () => {
     let bookstoreName = "Book World"
-    console.log(await addBookstore(bookstoreName))
+    console.log(await this.addBookstore(bookstoreName))
 }
 
-const ex17 = async () => {
+exports.ex17 = async () => {
     let newBookTitle = "It Begins Here"
     let newBookIsbn = "123-12-12344-22-111"
     let bookStoreId = 1
-    console.log(await addBook(newBookTitle, newBookIsbn, bookStoreId))
+    console.log(await this.addBook(newBookTitle, newBookIsbn, bookStoreId))
 }
 
 const main = async () => {
@@ -50,34 +62,61 @@ const main = async () => {
 // Your functions here...
 //
 
-const getPerson = async (personId) => {
+exports.getPerson = async (personId) => {
     let retval = null;
-    // TODO...
+    try{
+     let r = await pool.query(GET_PERSON, [personId]);
+     retval = r.rows;
+    } catch(err){
+        console.error(err)
+    }
     return retval;
 }
 
-const getBooks = async (bookStoreId) => {
+exports.getBooks = async (bookStoreId) => {
     let retval = null;
-    // TODO...
+    try{
+        let r = await pool.query(GET_BOOKS, [bookStoreId]);
+        retval = r.rows;
+       } catch(err){
+           console.error(err)
+       }
     return retval;
 }
 
-const updatePerson = async (personId, newName) => {
+exports.updatePerson = async (personId, newName) => {
     let retval = null;
-    // TODO...
+    try{
+        let r = await pool.query(updatePerson, [personId,newName]);
+        retval = r.rows;
+       } catch(err){
+           console.error(err)
+       }
     return retval;
 }
 
-const addBookstore = async (bookstoreName) => {
+exports.addBookstore = async (bookstoreName) => {
     let retval = null;
-    // TODO...
+    try{
+        let r = await pool.query(addBookstore, [bookstoreName]);
+        retval = r.rows;
+       } catch(err){
+           console.error(err)
+       }
     return retval;
 }
 
-const addBook = async (title, isbn, bookstoreId) => {
+exports.addBook = async (title, isbn, bookstoreId) => {
     let retval = null;
-    // TODO...
+    try {
+        let r = await pool.query(addBook, [title, isbn]);
+        let bookId = r.rows[0].book_id;
+        r = await pool.query(add_Book_Store_Book, [bookId, bookstoreId]);
+        retval = r.rows[0];
+      } catch (err) {
+        console.error(err);
+      }
     return retval;
 }
 
-main()
+//main()
